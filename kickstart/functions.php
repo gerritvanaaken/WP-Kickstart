857613e72b97d0c1d8aa897727c16646b8ff05cd
Gerrit van Aaken <gerritvanaaken@praegnanz.local>
2011-03-02 11:29:17 +0100

    Added dynamic id and class to Widget wrapper

--- kickstart/functions.php
+++ kickstart/functions.php
@@ -3,7 +3,7 @@ function kickstart_widgets_init() {
   register_sidebar(array(
     'name' => 'Widgets',
     'id' => 'widgets',
-    'before_widget' => '<div class="box">',
+    'before_widget' => '<div id="%1$s" class="box %2$s">',
     'after_widget' => '</div>',
     'before_title' => '<h2>',
     'after_title' => '</h2>'
