55d7b78f489e31a33dd0a57ff15b50c6d034602b
Gerrit van Aaken <gerrit@praegnanz.de>
2011-11-22 11:18:43 +0100

    killed robots line, updated jQuery

--- kickstart/header.php
+++ kickstart/header.php
@@ -14,9 +14,8 @@
   <link href="<?php bloginfo('template_directory') ?>/print.css" rel="stylesheet" media="print" type="text/css" />
   <script>document.documentElement.className += " js";</script>
   <script src="<?php bloginfo('template_directory') ?>/js/iepp.js"></script>
-  <script src="<?php bloginfo('template_directory') ?>/js/jquery-1.7.min.js"></script>
+  <script src="<?php bloginfo('template_directory') ?>/js/jquery-1.7.1.min.js"></script>
   <script src="<?php bloginfo('template_directory') ?>/js/domscript.js"></script>
-  <meta name="robots" content="index, follow" />
   <link rel="shortcut icon" href="<?php bloginfo('template_directory') ?>/img/favicon.ico" type="image/vnd.microsoft.icon" /> 
   <?php wp_head() ?>
 </head>
