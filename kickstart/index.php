8c7c3d6b477ec7ffaa55c577f6352e55887fc405
Gerrit van Aaken <gerrit@praegnanz.de>
2012-05-10 13:15:46 +0200

    added permalink to index.php

--- kickstart/index.php
+++ kickstart/index.php
@@ -3,7 +3,7 @@
 <?php if (have_posts()): ?>
 	<?php while(have_posts()): the_post() ?>
 	<div <?php post_class() ?>>
-		<h2><?php the_title() ?></h2>
+		<h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
 		<?php the_content() ?>
 	</div>
 	<?php endwhile; ?>
